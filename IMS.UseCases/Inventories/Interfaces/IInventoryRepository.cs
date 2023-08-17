145a6dac7c510391e7b74d3c97381ebec3fd07e0
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-17 15:56:46 +0300

    Add project files.

---
+++ IMS.UseCases/Inventories/Interfaces/IInventoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using IMS.CoreBusiness;
+
+namespace IMS.UseCases.Inventories.Interfaces
+{
+    public interface IInventoryRepository
+    {
+        Task<IEnumerable<Inventory>> GetInventoriesByNameAsync(string name);
+    }
+}
