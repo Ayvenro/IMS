064de857efb187f75ac74083baf7611e35fd2a06
Ayvenro <46055882+Ayvenro@users.noreply.github.com>
2023-08-20 18:01:26 +0300

    Implement simple data validation

--- IMS.CoreBusiness/Inventory.cs
+++ IMS.CoreBusiness/Inventory.cs
@@ -1,10 +1,15 @@
-﻿namespace IMS.CoreBusiness
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IMS.CoreBusiness
 {
     public class Inventory
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; } = string.Empty;
+        [Range(0, int.MaxValue, ErrorMessage = "Quantity must be greater or equal to 0")]
         public int Quantity { get; set; }
-        public decimal Price { get; set; }
+		[Range(0, int.MaxValue, ErrorMessage = "Price must be greater or equal to 0")]
+		public decimal Price { get; set; }
     }
 }
